172c0c978426fa622f83f8421c2f7423890f4587
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-04-19 12:41:59 -0500

    Script to enumerate files touched by an author

---
+++ scripts/git_author_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]
+then
+	echo "Usage:   ./git_author_files.sh <AUTHOR>"
+	echo "Example: ./git_author_files.sh \"Ian Hutchinson\""
+	exit 1
+fi
+
+git log --pretty="%H" --author="$1" |
+	while read HASH
+	do
+		git show --oneline --name-only $HASH | tail -n+2
+	done | sort | uniq
