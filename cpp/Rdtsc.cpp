5174f2abe953021132f8f078901ff89567e59291
Ian Hutchinson <ihutchinson@agalmicventures.com>
2022-01-03 21:28:55 -0600

    Update copyrights to 2022

--- cpp/Rdtsc.cpp
+++ cpp/Rdtsc.cpp
@@ -1,7 +1,7 @@
 //usr/bin/g++ "$0" -o /tmp/Rdtsc && exec /tmp/Rdtsc "$@"
 
 /*
- * Copyright (c) 2015-2021 Agalmic Ventures LLC (www.agalmicventures.com)
+ * Copyright (c) 2015-2022 Agalmic Ventures LLC (www.agalmicventures.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
